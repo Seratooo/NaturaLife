d24117efdd2131fff2e4dbac097c62d586572987
Abubacar Correia <pauloserato2016@gmail.com>
2023-02-05 16:29:34 +0100

    add rotes

---
+++ pages/plans.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from 'next';
+
+import Plans from '../views/plans';
+
+const PlansPage: NextPage = () => <Plans />;
+
+export default PlansPage;
---
+++ pages/register.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from 'next';
+
+import Register from '../views/register';
+
+const RegisterPage: NextPage = () => <Register />;
+
+export default RegisterPage;
