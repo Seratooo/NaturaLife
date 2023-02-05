71301bf3eaacb69113dfc851f3c4b6dcf5a50cb1
Abubacar Correia <pauloserato2016@gmail.com>
2023-02-05 16:27:54 +0100

    add input global styles

--- design-system/global-styles.ts
+++ design-system/global-styles.ts
@@ -4,11 +4,14 @@ const GlobalStyles = css`
   * {
     margin: 0;
     padding: 0;
+    font-size: 62.5%;
     box-sizing: border-box;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
       Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   }
-
+  ::-webkit-input-placeholder {
+    color: white;
+  }
   body {
     overflow-x: hidden;
   }
